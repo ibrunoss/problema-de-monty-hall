9188f073f1d829eda44ac1482197da396968560d
Bruno Silva Santana <ibrunoss@outlook.com>
2021-09-07 11:59:37 -0300

    Classe PortaModel criada

---
+++ model/PortaModel.ts
@@ -0,0 +1,60 @@
+export default class PortaModel {
+  #numero: number;
+  #temPresente: boolean;
+  #selecionada: boolean;
+  #aberta: boolean;
+
+  constructor(
+    numero: number,
+    temPresente = false,
+    selecionada = false,
+    aberta = false
+  ) {
+    this.#numero = numero;
+    this.#temPresente = temPresente;
+    this.#selecionada = selecionada;
+    this.#aberta = aberta;
+  }
+
+  get numero() {
+    return this.#numero;
+  }
+  get temPresente() {
+    return this.#temPresente;
+  }
+  get selecionada() {
+    return this.#selecionada;
+  }
+  get aberta() {
+    return this.#aberta;
+  }
+
+  cancelarSelecao() {
+    const selecionada = false;
+    return this.#selecao(selecionada);
+  }
+
+  alternarSelecao() {
+    const selecionada = !this.selecionada;
+    return this.#selecao(selecionada);
+  }
+
+  #selecao(selecionada) {
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      selecionada,
+      this.aberta
+    );
+  }
+
+  abrir() {
+    const aberta = true;
+    return new PortaModel(
+      this.numero,
+      this.temPresente,
+      this.selecionada,
+      aberta
+    );
+  }
+}
